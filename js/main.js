97f262e2d73dd5dcb7855f4a6dff82a860bceb82
henriquecardososouza <riquecardoso2018@outlook.com>
2025-04-08 23:19:11 -0300

    Corrigindo funcionamento no mobile

--- js/main.js
+++ js/main.js
@@ -32,16 +32,54 @@ document.getElementById("generate-btn").addEventListener("click", (e) => {
     }, 2000);
 });
 
-function downloadImage() {
-    const image = document.querySelector('#qr img');
+function downloadImage(button) {
+    button.innerHTML = `Downloading<span class="loader ms-2"></span>`;
+    button.disabled = true;
+    const img = document.querySelector('#qr img');
+
+    fetch(img.src)
+        .then(response => response.blob())
+        .then(blob => {
+            window.saveAs(blob, 'qr-code.png');
+        });
+
+    button.innerHTML = `Download`;
+
+    /*domtoimage.toBlob()
+        .then(function (blob) {
+            window.saveAs(blob, 'my-node.png');
+            button.innerHTML = `Download`;
+            button.disabled = false;
+        });*/
+    /*const image = document.querySelector('#qr img');
     const link = document.createElement('a');
-    link.style.display = 'none';
-    link.rel = 'noopener noreferrer';
+
+    console.log(image.src);
+    let byteCharacters = atob(image.src);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+        const slice = byteCharacters.slice(offset, offset + 512);
+
+        const byteNumbers = new Array(slice.length);
+        for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        const byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+    }
+
+    const type = "image/png";
+    const blob = new Blob(byteArrays, { type });
+    const file = new File([blob], "qrcode.png", { type });
     
     fetch(image.src)
         .then(response => response.blob())
         .then(blob => {
-            link.href = URL.createObjectURL(blob);
+            link.href = URL.createObjectURL(file);
+            link.style.display = 'none';
+            link.rel = 'noopener noreferrer';
             link.download = 'qrcode.png';
             link.type = 'image/png';
             link.target = '_blank';
@@ -55,5 +93,5 @@ function downloadImage() {
             link.dispatchEvent(clickEvent);
 
             setTimeout(() => URL.revokeObjectURL(link.href), 2000);
-        });
+       // });*/
 }
