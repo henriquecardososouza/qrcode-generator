d476f048184d49d2f85a03aef95670d28e1a80ba
henriquecardososouza <riquecardoso2018@outlook.com>
2025-04-08 23:31:09 -0300

    Corrigindo funcionamento no mobile

--- js/main.js
+++ js/main.js
@@ -35,68 +35,10 @@ document.getElementById("generate-btn").addEventListener("click", (e) => {
 function downloadImage(button) {
     button.innerHTML = `Downloading<span class="loader ms-2"></span>`;
     button.disabled = true;
-    // const img = document.querySelector('#qr img');
 
     document.querySelector("#qr canvas").toBlob(function(blob) {
         window.saveAs(blob, "qrcode.png");
+        button.innerHTML = `Download`;
+        button.disabled = false;
     });
-
-    // fetch(img.src)
-    //     .then(response => response.blob())
-    //     .then(blob => {
-    //         window.saveAs(blob, 'qr-code.png');
-    //     });
-
-    button.innerHTML = `Download`;
-    button.disabled = false;
-
-    /*domtoimage.toBlob()
-        .then(function (blob) {
-            window.saveAs(blob, 'my-node.png');
-            button.innerHTML = `Download`;
-            button.disabled = false;
-        });*/
-    /*const image = document.querySelector('#qr img');
-    const link = document.createElement('a');
-
-    console.log(image.src);
-    let byteCharacters = atob(image.src);
-    const byteArrays = [];
-
-    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
-        const slice = byteCharacters.slice(offset, offset + 512);
-
-        const byteNumbers = new Array(slice.length);
-        for (let i = 0; i < slice.length; i++) {
-            byteNumbers[i] = slice.charCodeAt(i);
-        }
-
-        const byteArray = new Uint8Array(byteNumbers);
-        byteArrays.push(byteArray);
-    }
-
-    const type = "image/png";
-    const blob = new Blob(byteArrays, { type });
-    const file = new File([blob], "qrcode.png", { type });
-    
-    fetch(image.src)
-        .then(response => response.blob())
-        .then(blob => {
-            link.href = URL.createObjectURL(file);
-            link.style.display = 'none';
-            link.rel = 'noopener noreferrer';
-            link.download = 'qrcode.png';
-            link.type = 'image/png';
-            link.target = '_blank';
-            document.body.appendChild(link);
-
-            const clickEvent = new MouseEvent('click', {
-                bubbles: true,
-                cancelable: true,
-                view: window
-            });
-            link.dispatchEvent(clickEvent);
-
-            setTimeout(() => URL.revokeObjectURL(link.href), 2000);
-       // });*/
 }
