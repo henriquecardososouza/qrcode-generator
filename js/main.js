9510fc793e0cf27b5d86062c92fe10355afe02c5
henriquecardososouza <riquecardoso2018@outlook.com>
2025-04-08 22:22:53 -0300

    Adicionando projeto

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let qrcode = undefined;
+
+document.getElementById("generate-btn").addEventListener("click", (e) => {
+    const link = document.getElementById("link").value.trim();
+    const button = document.getElementById("generate-btn");
+
+    if (link.length == 0) {
+        const toast = bootstrap.Toast.getOrCreateInstance(document.getElementById("error-toast"));
+        document.querySelector("#error-toast .toast-body").innerHTML = "Insira um link";
+        toast.show();
+        return;
+    }
+
+    button.innerHTML = `Gerando<span class="loader ms-2"></span>`;
+    button.disabled = true;
+    document.getElementById("qr").parentElement.classList.add("d-none");
+    document.getElementById("qr").innerHTML = "";
+
+    setTimeout(() => {
+        qrcode = new QRCode("qr", {
+            text: link,
+            width: 512,
+            height: 512,
+            colorDark : "#000000",
+            colorLight : "#ffffff",
+            correctLevel : QRCode.CorrectLevel.H
+        });
+
+        document.getElementById("qr").parentElement.classList.remove("d-none");
+        button.innerHTML = `Gerar`;
+        button.disabled = false;
+    }, 2000);
+});
+
+function downloadImage() {
+    const image = document.querySelector('#qr img');
+    const link = document.createElement('a');
+    
+    fetch(image.src)
+        .then(response => response.blob())
+        .then(blob => {
+            link.href = URL.createObjectURL(blob);
+            link.download = 'qrcode.png';
+            link.click();
+            setTimeout(() => URL.revokeObjectURL(link.href), 2000);
+        });
+}
